3f9ffd618b4ed48a9a764d1f5504b1fba9ffbb34
PilyaP <ppaexpert@gmail.com>
2023-06-12 19:11:06 +0300

    1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>YohooOfdddfgghy6uff!</Text>
+      <Text>YohooOfdddfgghy655uff!</Text>
       <StatusBar style="auto" />
     </View>
   );
